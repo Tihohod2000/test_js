9af5e69bedcdb90fe272c6b29a4663115312ab0b
Tihohod2000 <dumanovskiy15@gmail.com>
2025-06-10 23:49:24 +0300

    fix name

--- ex_2/back-end/src/config.ts
+++ ex_2/back-end/src/config.ts
@@ -2,6 +2,6 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const fileDirPath = `${process.cwd()}/uploads`;
+export const fileDirPath = `./uploads`;
 export const timeAlive: number = Number(process.env.TIME_ALIVE || 30) * 60 * 1000;
 export const port = process.env.PORT || 3000;
