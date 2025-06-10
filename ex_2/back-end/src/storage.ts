cbd52f6ea70c6ed735a202bc076a716b1c1f0cab
Tihohod2000 <dumanovskiy15@gmail.com>
2025-06-10 23:22:55 +0300

    fix create folder

--- ex_2/back-end/src/storage.ts
+++ ex_2/back-end/src/storage.ts
@@ -2,6 +2,12 @@ import multer from 'multer';
 import path from 'path';
 import {v4 as uuidv4} from 'uuid';
 import {fileDirPath} from './config';
+import fs from 'fs';
+
+if (!fs.existsSync(fileDirPath)) {
+    fs.mkdirSync(fileDirPath, { recursive: true }); // recursive: true создаст все вложенные папки
+    console.log(`Директория ${fileDirPath} создана`);
+}
 
 export let storage: multer.StorageEngine;
 storage = multer.diskStorage({
